bfdeb8e673c9083e733f1dd0e43f9c1e43f933b6
ladnik <niklasladurner@yahoo.com>
2023-09-29 14:56:16 +0200

    reposition footer

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 /* https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp */
 
 const darkModeToggle = document.getElementById('dark-mode-toggle');
+const slider = document.querySelector(".slider");
 
 function getUserThemePreference() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       darkModeToggle.checked = true;
-      darkModeToggle.click();
+      slider.classList.add("checked");
       document.documentElement.setAttribute('data-theme', 'dark');
     } else {
       darkModeToggle.checked = false;
+      slider.classList.remove("checked");
       document.documentElement.removeAttribute('data-theme');
     }
   }
 function switchTheme(e) {
-    console.log("switch\n");
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        slider.classList.add("checked");
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        slider.classList.remove("checked");
     }    
 }
 

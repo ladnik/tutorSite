fc7a52ce8ab104d8849ca54ac812a905c8d0d045
Niklas Ladurner <ladu@in.tum.de>
2023-11-10 22:56:40 +0100

    update perms script and added writeup & code for w03

--- perm.sh
+++ perm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod -R o+rx ../html-data
-find ../html-data/stuff -type f ! \( -name "*.pdf" -o -name "*.tex" -o -name "*.csv" \) -exec chmod o-rx {} \;
+find ../html-data/stuff -type f ! \( -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" \) -exec chmod o-rx {} \;

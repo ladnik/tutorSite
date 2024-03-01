9814e1031eea1ef7535b56ccb45a256cbed5df10
Niklas Ladurner <ladu@in.tum.de>
2024-03-01 20:27:51 +0100

    modify perms

--- perm.sh
+++ perm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-chmod -R o+rx ../html-data
-#chmod o+rx ../html-data
-find . -type f ! \( -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" -name "*.circ" \) -exec chmod o+rx {} \;
+chmod -R o-rwx stuff
+find . -type f \( -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" -o -name "*.circ" \) -exec chmod o+rx {} \;
+find stuff -type d -exec chmod o+rx {} \;
+chmod o-r stuff

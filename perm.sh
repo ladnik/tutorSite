43ec8055a162156ec7a32d741fe7ece3c9c0a594
Niklas Ladurner <ladu@in.tum.de>
2024-05-26 15:53:05 +0200

    update

--- perm.sh
+++ perm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod -R o-rwx stuff
-find . -type f \( -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" -o -name "*.circ" \) -exec chmod o+rx {} \;
+chmod o-rx stuff
+find . -type f \( -name "*.html" -o -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" -o -name "*.circ" -o -name "*.c" -o -name "*.asm" \) -exec chmod o+rx {} \;
 find stuff -type d -exec chmod o+rx {} \;
-chmod o-r stuff

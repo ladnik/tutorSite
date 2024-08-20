8243aafb43750bc9e9df40eb503b543f437df49d
ladnik <niklasladurner03@gmail.com>
2024-08-20 23:57:00 +0200

    Update perms script

--- perm.sh
+++ perm.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+week=$1
+prefixes=("era" "gra" "era2")
+types=("html" "pdf" "tex" "csv" "S" "circ" "c" "asm")
+
+for page in "$prefixes[@]"; do
+    find . -type f -name "${prefix}*.html" -exec chmod o+rx {} \;
+done
+
 chmod o-rx stuff
-find . -type f \( -name "*.html" -o -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" -o -name "*.circ" -o -name "*.c" -o -name "*.asm" \) -exec chmod o+rx {} \;
-find stuff -type d -exec chmod o+rx {} \;
+find stuff -exec chmod o-rwx {} \;
+
+for i in $(seq 1 $week); do
+    echo "Setting permissions for week $i"
+
+    week_pad=$(printf "%02d" $i)
+    for prefix in "${prefixes[@]}"; do
+        for type in "${types[@]}"; do
+            find stuff -type f -name "${prefix}*${week_pad}*.${type}" -exec chmod o+rx {} \;
+        done
+    done
+done
+
+echo "Setting permissions for bonus material"
+find stuff -type f -name "bn_*" -exec chmod o+rx {} \;
+
+echo "Done"
+
--- script.js
+++ script.js
@@ -4,28 +4,69 @@ const darkModeToggle = document.getElementById('dark-mode-toggle');
 const slider = document.querySelector(".slider");
 
 function getUserThemePreference() {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      darkModeToggle.checked = true;
-      slider.classList.add("checked");
-      document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      darkModeToggle.checked = false;
-      slider.classList.remove("checked");
-      document.documentElement.removeAttribute('data-theme');
-    }
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkModeToggle.checked = true;
+    slider.classList.add("checked");
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    darkModeToggle.checked = false;
+    slider.classList.remove("checked");
+    document.documentElement.removeAttribute('data-theme');
   }
+}
 function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        slider.classList.add("checked");
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        slider.classList.remove("checked");
-    }    
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    slider.classList.add("checked");
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    slider.classList.remove("checked");
+  }
 }
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-    getUserThemePreference();
+  getUserThemePreference();
 });
 darkModeToggle.addEventListener('change', switchTheme, false);
+
+// async function fetch_pwd() {
+//   try {
+//     const response = await fetch("sha");
+//     if (!response.ok) {
+//       throw new Error('error: ' + response.statusText);
+//     }
+//     const text = await response.text();
+//     return text.trim();
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
+// 
+// async function sha256(s) {
+//   let encoder = new TextEncoder();
+//   let data = encoder.encode(s);
+//   let buf = await crypto.subtle.digest('SHA-256', data);
+//   let arr = Array.from(new Uint8Array(buf));
+//   let hx = arr.map(byte => byte.toString(16).padStart(2, '0')).join('');
+//   return hx;
+// }
+// 
+// async function input_pwd(e) {
+//   e.preventDefault();
+//   let pwd = prompt("Enter password:")
+// 
+//   if (pwd === null) {
+//     return;
+//   }
+//   else if (await sha256(pwd) === await fetch_pwd()) {
+//     alert("OK!");
+//   }
+//   else {
+//     alert("Nope :(");
+//   }
+// 
+// }
+// 
+// const link = document.getElementById("promptLink");
+// link.addEventListener("click", input_pwd)

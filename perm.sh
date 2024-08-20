951f6297747354092321bc67420c3fd358ac180f
Niklas Ladurner <ladu@in.tum.de>
2024-08-20 23:59:31 +0200

    add deck

--- perm.sh
+++ perm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod o-rx stuff
-find . -type f \( -name "*.html" -o -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" -o -name "*.circ" -o -name "*.c" -o -name "*.asm" \) -exec chmod o+rx {} \;
+find . -type f \( -name "*.html" -o -name "*.pdf" -o -name "*.tex" -o -name "*.csv" -o -name "*.S" -o -name "*.circ" -o -name "*.c" -o -name "*.asm" -o -name "*.txt" -o -name "*.gpg" \) -exec chmod o+rx {} \;
 find stuff -type d -exec chmod o+rx {} \;

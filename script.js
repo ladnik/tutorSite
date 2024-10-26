8b436fdf8605751364f5b4cc6b4127b81a38f2a3
ladnik <niklasladurner03@gmail.com>
2024-10-27 00:16:12 +0200

    add cookies :)

--- script.js
+++ script.js
@@ -1,72 +1,60 @@
 /* https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp */
+/* https://www.w3schools.com/js/js_cookies.asp */
 
 const darkModeToggle = document.getElementById('dark-mode-toggle');
-const slider = document.querySelector(".slider");
+const slider = document.querySelector('.slider');
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 function getUserThemePreference() {
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    darkModeToggle.checked = true;
-    slider.classList.add("checked");
+  var themePreference = getCookie('theme')
+  if (themePreference === 'dark') {
     document.documentElement.setAttribute('data-theme', 'dark');
+    darkModeToggle.checked = true;
+    slider.classList.add('checked');
   } else {
+    document.documentElement.setAttribute('data-theme', 'light');
     darkModeToggle.checked = false;
-    slider.classList.remove("checked");
-    document.documentElement.removeAttribute('data-theme');
+    slider.classList.remove('checked');
   }
 }
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    slider.classList.add("checked");
+    slider.classList.add('checked');
+
+    const expires = new Date(Date.now() + 15552000000).toUTCString();
+    document.cookie = `theme=dark; expires=${expires}; path=/`;
   }
   else {
     document.documentElement.setAttribute('data-theme', 'light');
-    slider.classList.remove("checked");
+    slider.classList.remove('checked');
+
+    const expires = new Date(Date.now() + 15552000000).toUTCString();
+    document.cookie = `theme=light; expires=${expires}; path=/`;
   }
 }
 
+/*
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
   getUserThemePreference();
 });
+*/
+window.onload = function() {
+  getUserThemePreference();
+};
 darkModeToggle.addEventListener('change', switchTheme, false);
-
-// async function fetch_pwd() {
-//   try {
-//     const response = await fetch("sha");
-//     if (!response.ok) {
-//       throw new Error('error: ' + response.statusText);
-//     }
-//     const text = await response.text();
-//     return text.trim();
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
-// 
-// async function sha256(s) {
-//   let encoder = new TextEncoder();
-//   let data = encoder.encode(s);
-//   let buf = await crypto.subtle.digest('SHA-256', data);
-//   let arr = Array.from(new Uint8Array(buf));
-//   let hx = arr.map(byte => byte.toString(16).padStart(2, '0')).join('');
-//   return hx;
-// }
-// 
-// async function input_pwd(e) {
-//   e.preventDefault();
-//   let pwd = prompt("Enter password:")
-// 
-//   if (pwd === null) {
-//     return;
-//   }
-//   else if (await sha256(pwd) === await fetch_pwd()) {
-//     alert("OK!");
-//   }
-//   else {
-//     alert("Nope :(");
-//   }
-// 
-// }
-// 
-// const link = document.getElementById("promptLink");
-// link.addEventListener("click", input_pwd)
